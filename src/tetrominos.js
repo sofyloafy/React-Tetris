e00d9a14e8a701cf1dcf65d711a255f64d28898f
Sophie <sophiebrown438@gmail.com>
2020-08-05 11:10:28 +0100

    L and O shape tetrominos

--- src/tetrominos.js
+++ src/tetrominos.js
@@ -1,4 +1,5 @@
 export const TETROMINOS = {
+
   0: { shape: [[0]], color: '0, 0, 0' },
   I: { 
       shape:  [
@@ -19,4 +20,21 @@ export const TETROMINOS = {
       color: '36, 95, 223',
 
     },
+  L: { 
+      shape:  [
+                [0, 'L', 0],
+                [0, 'L', 0],
+                [0, 'L', 'L']
+              ],
+      color: '223, 173, 36',
+
+  },
+  O: { 
+    shape:  [
+              ['O', 'O'],
+              ['O', 'O']
+            ],
+    color: '223, 217, 36',
+
+},
 }
