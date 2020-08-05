3f7e2fb36f21c228633b5b158759bcae22ee73e1
Sophie <sophiebrown438@gmail.com>
2020-08-05 11:40:17 +0100

    Further cell styling

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -3,7 +3,7 @@ import { StyledCell } from './styles/StyledCell'
 import { TETROMINOS } from '../tetrominos';
 
 const Cell = ({ type }) => (
-  <div>cell</div>
+  <StyledCell type={'L'} color={TETROMINOS['L'].color} >cell</StyledCell>
 )
 
 export default Cell;
