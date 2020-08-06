d6508a4be8cda8215e85cae3082f709bd3fbf0c9
Sophie <sophiebrown438@gmail.com>
2020-08-06 11:12:53 +0100

    Debug playerRotate

--- src/hooks/usePlayer.js
+++ src/hooks/usePlayer.js
@@ -9,24 +9,28 @@ export const usePlayer = () => {
     collided: false,
   });
 
-  const rotate = (matrix, dir) => {
-    const rotatedTetro = matrix.map((_, index) => 
-    matrix.map(col => col[index]),
-    )
+  function rotate(matrix, dir) {
+    const rotatedTetro = matrix.map((_, index) =>matrix.map(col => col[index]))
 
     if (dir > 0) return rotatedTetro.map(row => row.reverse());
     return rotatedTetro.reverse();
   }
 
-  const playerRotate = (stage, dir) => {
-    const clonedPlayer = JSON.parse(JSON.stringify(player))
+  function playerRotate(stage, dir) {
+    const clonedPlayer = JSON.parse(JSON.stringify(player));
     clonedPlayer.tetromino = rotate(clonedPlayer.tetromino, dir);
-    const pos = clonedPlayer.pos.x 
-    let offset = 1
 
-    while (checkCollision(player, stage, {x: 0, y: 0})){
-      clonedPlayer.pos.x += offset
-      offset = -(offset + (offset > 0 ? 1 : -1))
+    const pos = clonedPlayer.pos.x;
+    let offset = 1;
+
+    while (checkCollision(clonedPlayer, stage, {x: 0, y: 0})){
+      clonedPlayer.pos.x += offset;
+      offset = -(offset + (offset > 0 ? 1 : -1));
+      if (offset > clonedPlayer.tetromino[0].length) {
+        rotate(clonedPlayer.tetromino, -dir);
+        clonedPlayer.pos.x = pos;
+        return;
+      }
     }
     setPlayer(clonedPlayer)
   }
--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -13,8 +13,6 @@ export const useStage = (player, resetPlayer) => {
       player.tetromino.forEach((row, y) => {
         row.forEach((value, x) => {
           if (value !== 0){
-            console.log(newStage[y + player.pos.y][x + player.pos.x])
-
             newStage[y + player.pos.y][x + player.pos.x] = [
               value,
               `${player.collided ? 'merged' : 'clear'}`,
